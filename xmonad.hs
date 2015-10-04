e0da7e2439b9bbdd4a8ccdb73c5ca6652fa600f6
Peter Halliday <pghalliday@gmail.com>
2015-10-04 10:16:12 +0200

    add set working directory for layouts

--- xmonad.hs
+++ xmonad.hs
@@ -1,16 +1,20 @@
 import XMonad
+import XMonad.Prompt
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.WorkspaceDir
 import System.IO
+
+myLayout = workspaceDir "~" (avoidStruts $ layoutHook defaultConfig)
 
 main = do
   xmproc <- spawnPipe "xmobar ~/.xmonad/.xmobarrc"
   xmonad $ defaultConfig
     { terminal = "urxvt"
     , manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , layoutHook = myLayout
     , logHook = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
       , ppTitle = xmobarColor "green" "" . shorten 50
@@ -19,4 +23,5 @@ main = do
     [ ((mod1Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
     , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
     , ((0, xK_Print), spawn "scrot")
+    , ((mod1Mask .|. shiftMask, xK_x), changeDir defaultXPConfig)
     ]
